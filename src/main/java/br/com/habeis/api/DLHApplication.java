a5c916918727ea9e7c2f5bd062b1e6462df147c8
Diego Dulval <diegodifreitas@gmail.com>
2018-02-27 10:16:32 -0300

    Added boot class

---
+++ src/main/java/br/com/habeis/api/DLHApplication.java
@@ -0,0 +1,38 @@
+package br.com.habeis.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class DLHApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DLHApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+       
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        //jogando a ordem lá para baixo, tem que rodar do filtro do security.
+        bean.setOrder(0);
+        return bean;
+    }
+}
