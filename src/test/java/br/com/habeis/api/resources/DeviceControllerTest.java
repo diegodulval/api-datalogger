1f9050543858c9abbd9761183f59ae9cdd461fe5
Diego Dulval <diegodifreitas@gmail.com>
2018-03-21 15:04:51 -0300

    Alter properties for test configuration

---
+++ src/test/java/br/com/habeis/api/resources/DeviceControllerTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.habeis.api.resources;
+
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"security.basic.enabled=false"})
+public class DeviceControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getDevice()
+            throws Exception {
+
+        mvc.perform(get("/device")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("content.[0].name", is("Placa #1")));
+
+        //mvc.perform(post("/increaseSalary").param("id", emp.getId() + "")).andExpect(status().is(200));
+    }
+}
