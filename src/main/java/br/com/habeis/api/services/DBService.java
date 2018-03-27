b478cada21f5de985f1aaf2c14b2b351aeaa8523
diegodulval <diegodifreitas@gmail.com>
2018-03-27 09:04:11 -0300

    Updated DBService

--- src/main/java/br/com/habeis/api/services/DBService.java
+++ src/main/java/br/com/habeis/api/services/DBService.java
@@ -22,8 +22,8 @@ public class DBService {
     public void instantiateTestDatabase() throws ParseException {
 
         Device device = new Device();
-        device.setDescription("Placa número #1");
-        device.setName("Placa #1");
+        device.setDescription("Equipamento #1");
+        device.setName("Equipamento #1");
         device.setId(1);
 
         device = deviceService.create(device);
@@ -126,17 +126,133 @@ public class DBService {
 
         Sensor sensor9 = new Sensor();
         sensor9.setDescription("Sensor número #9");
-        sensor9.setName("Sensor #9");
+        sensor9.setName("Gaveta");
         sensor9.setId(9);
         sensor9.setDevice(device);
         sensorService.create(sensor9);
 
         Sensor sensor10 = new Sensor();
         sensor10.setDescription("Sensor número #10");
-        sensor10.setName("Sensor #10");
+        sensor10.setName("Gaveta");
         sensor10.setId(10);
         sensor10.setDevice(device);
         sensorService.create(sensor10);
 
+        Device device1 = new Device();
+        device1.setDescription("Equipamento #2");
+        device1.setName("Equipamento #1");
+        device1.setId(1);
+
+        device1 = deviceService.create(device1);
+
+        Output output11 = new Output();
+        output11.setDescription("Saída número #1");
+        output11.setName("Saida #1");
+        //output11.setId(1);
+        output11.setDevice(device1);
+        output11.setStatus(00);
+        outputService.create(output11);
+
+        Output output12 = new Output();
+        output12.setDescription("Saída número #2");
+        output12.setName("Saida #2");
+        //output12.setId(2);
+        output12.setStatus(00);
+        output12.setDevice(device);
+        outputService.create(output12);
+
+        Output output13 = new Output();
+        output13.setDescription("Saída número #3");
+        output13.setName("Saida #3");
+        //output13.setId(3);
+        output13.setStatus(00);
+        output13.setDevice(device1);
+        outputService.create(output13);
+
+        Output output14 = new Output();
+        output14.setDescription("Saída número #4");
+        output14.setName("Saida #4");
+        //output14.setId(4);
+        output14.setStatus(00);
+        output14.setDevice(device1);
+        outputService.create(output14);
+
+        Output output15 = new Output();
+        output15.setDescription("Saída número #5");
+        output15.setName("Saida #5");
+        //output15.setId(5);
+        output15.setStatus(00);
+        output15.setDevice(device1);
+        outputService.create(output15);
+
+        Sensor sensor11 = new Sensor();
+        sensor11.setDescription("Sensor número #1");
+        sensor11.setName("Sensor #1");
+        //sensor11.setId(1);
+        sensor11.setDevice(device1);
+        sensorService.create(sensor11);
+
+        Sensor sensor12 = new Sensor();
+        sensor12.setDescription("Sensor número #2");
+        sensor12.setName("Sensor #2");
+        //sensor12.setId(2);
+        sensor12.setDevice(device1);
+        sensorService.create(sensor12);
+
+        Sensor sensor13 = new Sensor();
+        sensor13.setDescription("Sensor número #3");
+        sensor13.setName("Sensor #3");
+        //sensor13.setId(3);
+        sensor13.setDevice(device1);
+        sensorService.create(sensor13);
+
+        Sensor sensor14 = new Sensor();
+        sensor14.setDescription("Sensor número #4");
+        sensor14.setName("Sensor #4");
+        //sensor14.setId(4);
+        sensor14.setDevice(device1);
+        sensorService.create(sensor14);
+
+        Sensor sensor15 = new Sensor();
+        sensor15.setDescription("Sensor número #5");
+        sensor15.setName("Sensor #5");
+        //sensor15.setId(5);
+        sensor15.setDevice(device1);
+        sensorService.create(sensor15);
+
+        Sensor sensor16 = new Sensor();
+        sensor16.setDescription("Sensor número #6");
+        sensor16.setName("Sensor #6");
+        //sensor16.setId(6);
+        sensor16.setDevice(device1);
+        sensorService.create(sensor16);
+
+        Sensor sensor17 = new Sensor();
+        sensor17.setDescription("Sensor número #7");
+        sensor17.setName("Sensor #7");
+        //sensor17.setId(7);
+        sensor17.setDevice(device1);
+        sensorService.create(sensor17);
+
+        Sensor sensor18 = new Sensor();
+        sensor18.setDescription("Sensor número #8");
+        sensor18.setName("Sensor #8");
+        // sensor18.setId(8);
+        sensor18.setDevice(device1);
+        sensorService.create(sensor18);
+
+        Sensor sensor19 = new Sensor();
+        sensor19.setDescription("Sensor número #9");
+        sensor19.setName("Gaveta");
+        //sensor19.setId(9);
+        sensor19.setDevice(device1);
+        sensorService.create(sensor19);
+
+        Sensor sensor110 = new Sensor();
+        sensor110.setDescription("Sensor número #10");
+        sensor110.setName("Gaveta");
+        //sensor110.setId(10);
+        sensor110.setDevice(device1);
+        sensorService.create(sensor110);
     }
 }
